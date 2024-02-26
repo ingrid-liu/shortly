b9dda8f2cc074238285c97f03fcea80a88c9e1b9
Ingrid Liu <todearannie@gmail.com>
2024-02-25 18:14:24 -0800

    Add url shortener widget
    This commit adds a new UI widget to the app that allows the user to
    shorten a URL. The widget is a form that takes a URL as input and
    returns a shortened URL (not using actual API yet).
    
    Also add a new entry widget for the shortened URL.

---
+++ lib/widget/shortened_url_entry.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shortly/global/color.dart';
+import 'package:shortly/widget/button.dart';
+
+class ShortenedUrlEntry extends StatefulWidget {
+  final String rawUrl;
+  final String shortenedUrl;
+
+  const ShortenedUrlEntry({
+    super.key,
+    required this.rawUrl,
+    required this.shortenedUrl,
+  });
+
+  @override
+  State<ShortenedUrlEntry> createState() => _ShortenedUrlEntryState();
+}
+
+class _ShortenedUrlEntryState extends State<ShortenedUrlEntry> {
+  bool _copied = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.only(top: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: Flex(
+          direction: Axis.horizontal,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              widget.rawUrl,
+              style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                    color: CustomColors.neutralVeryDarkViolet,
+                  ),
+            ),
+            Flex(direction: Axis.horizontal, children: [
+              Text(
+                widget.shortenedUrl,
+                style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                      color: CustomColors.primaryCyan,
+                    ),
+              ),
+              const SizedBox(width: 30),
+              Button(
+                displayText: _copied ? 'copied!' : "Copy",
+                radius: const Radius.circular(5),
+                textStyle: Theme.of(context).textTheme.bodySmall!.copyWith(
+                      color: Colors.white,
+                    ),
+                onPressed: onCopy,
+                clickedColor: CustomColors.primaryDarkViolet,
+              ),
+            ]),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onCopy() {
+    Clipboard.setData(ClipboardData(text: widget.shortenedUrl)).then((_) {});
+    setState(() {
+      _copied = true;
+    });
+  }
+}
