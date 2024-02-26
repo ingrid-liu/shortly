ba1e930ba7ee2d32d6de62a5eaa893f263e46598
Ingrid Liu <todearannie@gmail.com>
2024-02-25 18:31:32 -0800

    Add a boost banner
    This commit creates a boost banner. It is a simple widget that can be
    used to display a boost message.

---
+++ lib/widget/boost.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shortly/global/color.dart';
+import 'package:shortly/widget/button.dart';
+
+class Boost extends StatelessWidget {
+  const Boost({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          width: double.infinity,
+          height: 250,
+          decoration: const BoxDecoration(
+            color: CustomColors.neutralVeryDarkBlue,
+          ),
+        ),
+        SvgPicture.asset(
+          'assets/images/bg-boost-desktop.svg',
+          fit: BoxFit.cover,
+        ),
+        Column(
+          children: [
+            Text(
+              'Boost your links today',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Colors.white,
+                  ),
+            ),
+            const SizedBox(height: 20),
+            const Button(
+              displayText: "Get Started",
+              radius: Radius.circular(25),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
